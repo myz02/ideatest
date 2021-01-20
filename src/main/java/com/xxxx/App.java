6a1addd081b5e2e7cdeead0c0d0c8391367d293e
myz <2980549941@qq.com>
2021-01-20 21:47:05 +0800

    master初始化结束

--- src/main/java/com/xxxx/App.java
+++ src/main/java/com/xxxx/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println("这他妈初始化个毛");
         System.out.println( "初始化操作" );
         System.out.println("份子初始化");
+        System.out.println("master初始化结束");
     }
 }
